6cb2dc9d2182c1282e93f4bcd7814559fc6e0590
vimalaanthony <vimalamca1991@gmail.com>
2022-08-20 10:48:13 +0530

    7 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { WeatherInfo, WEATHERDATA } from './data';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
   name = 'Angular';
   public weatheData: WeatherInfo[] = WEATHERDATA;
-  public searchKey: string;
+  public searchKey: string = 'denver';
+  public cityWeather: WeatherInfo = WEATHERDATA[0];
+  public isSearching: boolean = false;
 
   public search(): void {
     console.log('searfcj', this.searchKey);
     console.log('searfcj', this.weatheData);
-    const cityWeather = this.weatheData.find(
-      (item) => item.name === this.searchKey
+    this.cityWeather = this.weatheData.find(
+      (item) =>
+        item.name.toLocaleLowerCase() === this.searchKey.toLocaleLowerCase()
     );
-    console.log(cityWeather);
+    console.log(this.cityWeather);
   }
 }
--- src/app/weather-details/weather-details.component.ts
+++ src/app/weather-details/weather-details.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  Input,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 import { WeatherInfo } from '../data';
 @Component({
   selector: 'my-weather-details',
   templateUrl: './weather-details.component.html',
   styleUrls: ['./weather-details.component.css'],
 })
-export class WeatherDetailComponent implements OnChanges {
-  @Input() cityWeather: WeatherInfo;
+export class WeatherDetailComponent implements OnChanges, OnInit {
+  @Input() cityWeather: WeatherInfo = null;
+  // @Input() isSearching: boolean;
 
-  public ngOnChanges(): void {
-    console.log('weather', this.cityWeather);
+  public ngOnInit(): void {
+    console.log('weather Onint', this.cityWeather);
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['weather']) {
+      console.log('weather', this.cityWeather);
+    }
   }
 }
