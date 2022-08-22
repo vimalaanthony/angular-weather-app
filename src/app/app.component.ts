53787cc541accd1dcfc408a05820221a1adaa901
vimalaanthony <vimalamca1991@gmail.com>
2022-08-22 11:41:36 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,65 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { WeatherInfo, WEATHERDATA } from './data';
+import { Observable, Subject, of } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   // encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Angular';
   public weatheData: WeatherInfo[] = WEATHERDATA;
-  public searchKey: string = 'denver';
-  public cityWeather: WeatherInfo = WEATHERDATA[0];
+  public searchKey: string = '';
+  public cityWeather1: WeatherInfo;
+  public cityWeather: any = null;
   public isSearching: boolean = false;
+  public subject: Subject<any> = new Subject();
+  seachTimeout;
 
+  public ngOnInit(): void {
+    //way 2
+    this.cityWeather = this.subject
+      .pipe(
+        debounceTime(500),
+        map((searchText) => {
+          console.log('***', searchText);
+          const res = this.weatheData.find(
+            (item) =>
+              item.name.toLocaleLowerCase() === searchText.toLocaleLowerCase()
+          );
+          return res;
+        })
+      )
+      .subscribe((res) => {
+        this.cityWeather = res;
+        console.log('***', this.cityWeather);
+      });
+  }
   public search(): void {
+    // this.jsWay1();
+    this.jsWay2();
+  }
+  public jsWay2(): void {
+    console.log('this.searchKey', this.searchKey);
+    this.subject.next(this.searchKey);
+  }
+  public jsWay1(): void {
+    if (this.seachTimeout) {
+      clearInterval(this.seachTimeout);
+    }
+    this.seachTimeout = setTimeout(() => {
+      this.searchAction();
+    }, 2000);
+  }
+
+  public searchAction(): void {
     console.log('searfcj', this.searchKey);
-    console.log('searfcj', this.weatheData);
-    this.cityWeather = this.weatheData.find(
+    this.cityWeather1 = this.weatheData.find(
       (item) =>
         item.name.toLocaleLowerCase() === this.searchKey.toLocaleLowerCase()
     );
-    console.log(this.cityWeather);
+    console.log('res', this.cityWeather);
   }
 }
